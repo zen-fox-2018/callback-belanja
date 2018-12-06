c80cd4a992f6602b5719266d409b82ddb9943ad9
LuthfiArfi <luthfi.arfi@gmail.com>
2018-12-06 15:47:53 +0700

    commit

--- beli.js
+++ beli.js
@@ -4,10 +4,10 @@ function beli(uang, obj, cb){
     let kembalian = uang - obj.harga
     if (kembalian > 0) {
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
+      cb(kembalian);
     }else{
       console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+      cb(0);
     }
   }, obj.waktu);
 }
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 //your code here
+const beli = require('./beli.js');
+var item1 = {"item" : "roti", "harga" : 10000, "waktu" : 1000};
+var item2 = {"item" : "ayam", "harga" : 12000, "waktu" : 2000};
+var item3 = {"item" : "daging", "harga" : 13000, "waktu" : 1500};
+var item4 = {"item" : "sayur", "harga" : 10500, "waktu" : 1300};
+var item5 = {"item" : "minyak tanah", "harga" : 20000, "waktu" : 1200};
+var uang = 100000
+if (item1.harga > uang) {
+  console.log('uang tidak cukup');
+}
+else{
+  beli(uang, item1 , function(kembalian){
+    if (item2.harga > uang) {
+      console.log('uang tidak cukup');
+    }
+    else{
+      beli(kembalian, item2, function(kembalian){
+        if (item3.harga > uang) {
+          console.log('uang tidak cukup');
+        }
+        else{
+        beli(kembalian, item3, function(kembalian){
+          if (item1.harga > uang) {
+            console.log('uang tidak cukup');
+          }
+          else{
+          beli(kembalian, item4, function(kembalian){
+            if (item1.harga > uang) {
+              console.log('uang tidak cukup');
+            }
+            else{
+            beli(kembalian, item5, function(kembalian){
+              console.log('sisa kembalianya : '+kembalian);
+            })}
+          })}
+        })}
+      })}
+    }
+  )
+}
