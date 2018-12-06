42298af283b63fae1e653d22b7c8bc50593ea057
Januarsmad <januarsmad@gmail.com>
2018-12-06 16:19:10 +0700

    done

--- beli.js
+++ beli.js
@@ -1,13 +1,13 @@
 function beli(uang, obj, cb){
   console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    if (uang >= obj.harga) {
+      let kembalian = uang - obj.harga
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+    }else if(uang < obj.harga){
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`);
+      cb(uang)
     }
   }, obj.waktu);
 }
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //your code here
+const beli = require('./beli')
+
+const permen = { item: 'permen', harga: 1000, waktu: 1000 }
+const roti = { item: 'roti', harga: 2000, waktu: 1000 }
+const coklat = { item: 'coklat', harga: 8000, waktu: 1000 }
+const susu = { item: 'susu', harga: 5000, waktu: 1000 }
+const makaroni = { item: 'makaroni', harga: 3000, waktu: 1000 }
+
+beli(10000, permen, function(kembalian){
+    beli(kembalian, roti, function(kembalian) {
+        beli(kembalian, coklat, function(kembalian) {
+            beli(kembalian, susu, function(kembalian) {
+                beli(kembalian, makaroni, function(kembalian) {
+                    console.log(`sisa uang ${kembalian}`)
+                })
+            })
+        })
+    })
+})
