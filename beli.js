b5c7a151182bb57c0bef2b8231ebfcf1040dbc86
Celyn Susanto <celyn.vy@gmail.com>
2018-12-06 17:18:26 +0700

    belanja

--- beli.js
+++ beli.js
@@ -6,7 +6,7 @@ function beli(uang, obj, cb){
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
     }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`);
       cb(0)
     }
   }, obj.waktu);
