e742d41b06360e3ec4665c3c75e49326767e5430
patriagani <patria.gani@gmail.com>
2018-12-06 14:53:19 +0700

    callback belanja done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-//your code here
+const beli = require('./beli.js')
+
+let rokok = {
+  item: "rokok",
+  harga: 1000
+}
+
+let mekdi = {
+  item: "mekdi",
+  harga: 1500
+}
+
+let minum = {
+  item: "minum",
+  harga: 500
+}
+
+let permen = {
+  item: "permen",
+  harga: 200
+}
+
+beli(10000, rokok, function(uang) {
+  beli(uang, mekdi, function(uang) {
+    beli(uang, minum, function(uang) {
+      beli(uang, permen, function(uang) {
+        return console.log(`selesai belanja saatnya berpesta`);
+      })
+    })
+  })
+})
