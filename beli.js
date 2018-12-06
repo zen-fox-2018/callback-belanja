4e567c76f36d798a70a3c485ec787d2396905efa
Arief Rachman <arievrchman@gmail.com>
2018-12-06 15:42:36 +0700

    done

--- beli.js
+++ beli.js
@@ -1,14 +1,20 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
+function beli(uang, obj, cb) {
+
+  console.log(`Saya pergi membeli ${obj.item}`);
+
+  setTimeout(function () {
+
+    let kembalian = uang - obj.harga;
+
     if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
+      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);;
+      cb(kembalian);
+    } else {
+      kembalian = uang
       console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+      cb(kembalian);
     }
+
   }, obj.waktu);
 }
 
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 //your code here
+const beli = require('./beli.js');
+
+let uang = 100000;
+
+const americanno = {
+    item: 'Americanno',
+    harga: 10000,
+    waktu: 2000
+};
+
+const espresso = {
+    item: 'Espresso Khusus Sultan', 
+    harga: 600000,
+    waktu: 5000
+};
+
+const cappucino = {
+    item: 'Cappucino',
+    harga: 12000,
+    waktu: 3000
+};
+
+const greentea = {
+    item: 'Green Tea',
+    harga: 6000,
+    waktu: 2000
+};
+
+const water = {
+    item: 'Air Mineral',
+    harga: 2000,
+    waktu: 1000
+};
+
+beli(uang, americanno, function (kembalian) {
+    beli(kembalian, espresso, function (kembalian) {
+        beli(kembalian, cappucino, function (kembalian) {
+            beli(kembalian, greentea, function (kembalian) {
+                beli(kembalian, water, function (kembalian) {
+                });
+            });
+        });
+    });
+});
