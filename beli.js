f86460d509aa9a45abdf0f5b60589f3db2bcc2a6
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-12-06 16:06:43 +0700

    submit task

--- beli.js
+++ beli.js
@@ -1,12 +1,12 @@
-function beli(uang, obj, cb){
+function beli(uang, obj, cb) {
   console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+  setTimeout(function () {
+    let kembalian = uang - obj.harga;
+    if (kembalian > obj.harga) {
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+    } else {
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`);
       cb(0)
     }
   }, obj.waktu);
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-//your code here
+const beli = require('./beli.js');
+
+let bonbon = {
+  item: 'bon-bon',
+  waktu: 1000,
+  harga: 2000
+};
+
+let cake = {
+  item: 'cake',
+  waktu: 5000,
+  harga: 25000
+};
+
+let mie = {
+  item: 'mie goreng',
+  waktu: 2000,
+  harga: 10000
+};
+
+let eskrim = {
+  item: 'eskrim',
+  waktu: 1500,
+  harga: 8000,
+};
+
+let buah = {
+  item: 'buah',
+  waktu: 1000,
+  harga: 15000
+};
+
+beli(10000, bonbon, function (kembalian) {
+  if (kembalian > 0) {
+    beli(kembalian, cake, function (kembalian) {
+      if (kembalian > 0) {
+        beli(kembalian, mie, function (kembalian) {
+          if (kembalian > 0) {
+            beli(kembalian, eskrim, function (kembalian) {
+              if (kembalian > 0) {
+                beli(kembalian, buah, function (kembalian) {
+                  console.log(kembalian);
+                })
+              }
+            })
+          } 
+        })
+      }
+    })
+  }
+})
