8e38679bfd9349737dd70ca5725ead850798b943
Zhang <zhang@ZhangMBP15.local>
2018-12-06 15:25:30 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-//your code here
+const beli = require('./beli')
+let permen =  {
+  item: 'Permen',
+  harga: 2000,
+  waktu: 1000
+}
+let pita =  {
+  item: 'Pita',
+  harga: 3000,
+  waktu: 1000
+}
+let baju =  {
+  item: 'Baju',
+  harga: 5000,
+  waktu: 1000
+}
+let celana =  {
+  item: 'Celana',
+  harga: 3500,
+  waktu: 1000
+}
+let makanan =  {
+  item: 'Makanan',
+  harga: 4000,
+  waktu: 1000
+}
+
+beli(10000, permen, function(kembalian) {
+  beli(kembalian, makanan , function(kembalian) {
+    beli (kembalian, baju , function(kembalian) {
+      beli (kembalian, pita, function(kembalian) {
+        beli (kembalian, celana, function (kembalian) {
+          console.log(`Selesai belanja uang sisa tinggal ${kembalian}`)
+        })
+      })
+    })
+  })
+})
+
