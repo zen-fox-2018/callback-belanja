2564ee0a8aa42cfed0da0bd0c59796e16e3b3708
Mahdihrs <mahdiharis@outlook.com>
2018-12-06 15:09:26 +0700

    Callback belanja done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-//your code here
+const beli = require('./beli.js')
+
+beli(150000, {item: 'Mobil', harga: 50000,waktu: 3000}, function(kembalian){
+    beli(kembalian, {item: 'Motor',harga: 25000,waktu: 2000}, function(kembalian){
+        beli(kembalian, {item: 'Sepeda', harga: 10000, waktu: 1000}, function(kembalian) {
+            beli(kembalian, {item: 'Telepon Fax', harga: 7500, waktu: 500}, function(kembalian){
+                beli(kembalian, {item: 'Disket', harga: 5000, waktu: 750}, function(kembalian){
+                    beli(kembalian, {item: 'Kaset', harga: 2500, waktu: 850}, function(kembalian){
+                        if (kembalian === 0) {
+                            console.log(`Uang sudah habis, tidak ada kembalian sama sekali`)
+                        } else {
+                            console.log(`Jangan lupa tabung sisa uang kamu ya, uang kamu masih ada ${kembalian}.`)
+                        }
+                    })
+                })
+            })
+        })
+    })
+})
